8dca3a6475221902fcdbfca5ee58909753d43a06
Serhii Sharyi <serhiisharyi77@gmail.com>
2026-07-30 18:29:34 +0300

    feat: implement LED fade effect in main loop

--- src/main.c
+++ src/main.c
@@ -1,15 +1,22 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "ledc_led/ledc_led.h"
+#include "esp_log.h"
 
 void app_main(void) {
-    ESP_LOGI("LED", "Duty set to: %lu", (unsigned long)LED_BRIGHTNESS_50_PERCENT);
     ledc_led_init();
-    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LED_BRIGHTNESS_50_PERCENT);
-    ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-
 
     while (1) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        for (int duty = 0; duty <= 8191; duty += 250) {
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, duty);
+            ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+            vTaskDelay(pdMS_TO_TICKS(25));
+        }
+
+        for (int duty = 8191; duty >= 0; duty -= 250) {
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, duty);
+            ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+            vTaskDelay(pdMS_TO_TICKS(25));
+        }
     }
 }
